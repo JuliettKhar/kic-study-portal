d50d63e66d42f851451443f5b9c638d189d2f15c
julia_kh <juliettkhar@gmail.com>
2026-08-31 23:06:51 +0900

    fix formatting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,12 +2,9 @@
 import withNuxt from './.nuxt/eslint.config.mjs'
 import eslintConfigPrettier from 'eslint-config-prettier/flat'
 
-export default withNuxt(
-  eslintConfigPrettier,
-  {
-    rules: {
-      eqeqeq: ['error', 'always'],
-      'no-console': ['warn', { allow: ['warn', 'error'] }],
-    },
+export default withNuxt(eslintConfigPrettier, {
+  rules: {
+    eqeqeq: ['error', 'always'],
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
   },
-)
+})
